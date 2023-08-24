e1a907793f70b3af68e42984410b3c11bd1f8a02
Piotr Rozpończyk <rozpo.dev@gmail.com>
2023-08-24 15:28:00 +0100

    Add UTs for isMale

--- test/pesel_test.dart
+++ test/pesel_test.dart
@@ -2,20 +2,36 @@ import 'package:pesel/pesel.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Pesel()', () {
-    group('throws assert when PESEL is', () {
-      test('too short', () {
-        expect(() => Pesel('1234567890'), throwsA(isA<AssertionError>()));
-      });
+  group('Pesel() throws assert when PESEL is', () {
+    test('too short', () {
+      expect(() => Pesel('1234567890'), throwsA(isA<AssertionError>()));
+    });
 
-      test('too long', () {
-        expect(() => Pesel('123456789012'), throwsA(isA<AssertionError>()));
-      });
+    test('too long', () {
+      expect(() => Pesel('123456789012'), throwsA(isA<AssertionError>()));
     });
   });
 
   group('Pesel().isValid returns', () {
     test('true', () => expect(Pesel('22291028314').isValid, isTrue));
     test('false', () => expect(Pesel('12345678901').isValid, isFalse));
+  });
+
+  group('Pesel().isMale', () {
+    group('returns true when', () {
+      test('digit is 1', () => expect(Pesel('00000000010').isMale, isTrue));
+      test('digit is 3', () => expect(Pesel('00000000030').isMale, isTrue));
+      test('digit is 5', () => expect(Pesel('00000000050').isMale, isTrue));
+      test('digit is 7', () => expect(Pesel('00000000070').isMale, isTrue));
+      test('digit is 9', () => expect(Pesel('00000000090').isMale, isTrue));
+    });
+
+    group('returns false when', () {
+      test('digit is 0', () => expect(Pesel('00000000000').isMale, isFalse));
+      test('digit is 2', () => expect(Pesel('00000000020').isMale, isFalse));
+      test('digit is 4', () => expect(Pesel('00000000040').isMale, isFalse));
+      test('digit is 6', () => expect(Pesel('00000000060').isMale, isFalse));
+      test('digit is 8', () => expect(Pesel('00000000080').isMale, isFalse));
+    });
   });
 }
