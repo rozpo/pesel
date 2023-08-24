f46e10e1c46c5d63f29c778976a240aecc2980f6
Piotr Rozpończyk <rozpo.dev@gmail.com>
2023-08-24 11:43:53 +0100

    Create new dart package project

---
+++ example/pesel_example.dart
@@ -0,0 +1,6 @@
+import 'package:pesel/pesel.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/pesel_test.dart
@@ -0,0 +1,16 @@
+import 'package:pesel/pesel.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
