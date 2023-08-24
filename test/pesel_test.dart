78bad9c609b13dba1e2f0cd7e6c2ac5adbf8ca4d
Piotr Rozpończyk <rozpo.dev@gmail.com>
2023-08-24 16:10:19 +0100

    Add UTs for toString()

--- test/pesel_test.dart
+++ test/pesel_test.dart
@@ -2,19 +2,25 @@ import 'package:pesel/pesel.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Pesel() throws assert when PESEL is', () {
-    test('too short', () {
-      expect(() => Pesel('1234567890'), throwsA(isA<AssertionError>()));
-    });
-
-    test('too long', () {
-      expect(() => Pesel('123456789012'), throwsA(isA<AssertionError>()));
+  group('Pesel()', () {
+    group('throws assert when PESEL is', () {
+      test('too short', () {
+        expect(() => Pesel('1234567890'), throwsA(isA<AssertionError>()));
+      });
+      test('too long', () {
+        expect(() => Pesel('123456789012'), throwsA(isA<AssertionError>()));
+      });
     });
   });
 
-  group('Pesel().isValid returns', () {
-    test('true', () => expect(Pesel('22291028314').isValid, isTrue));
-    test('false', () => expect(Pesel('12345678901').isValid, isFalse));
+  group('Pesel().toString()', () {
+    test('returns PESEL number formatted to String',
+        () => expect(Pesel('11111111111').toString(), equals('11111111111')));
+  });
+
+  group('Pesel().isValid', () {
+    test('returns true', () => expect(Pesel('22291028314').isValid, isTrue));
+    test('returns false', () => expect(Pesel('12345678901').isValid, isFalse));
   });
 
   group('Pesel().isMale', () {
