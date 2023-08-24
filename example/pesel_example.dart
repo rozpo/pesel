275b7083dd05a1deb463745b9c7fcc2815555b74
Piotr Rozpończyk <rozpo.dev@gmail.com>
2023-08-24 12:07:51 +0100

    Prepare example of usage

--- example/pesel_example.dart
+++ example/pesel_example.dart
@@ -1,6 +1,30 @@
 import 'package:pesel/pesel.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  // Create a Pesel instance
+  final peselString = "22291028314";
+  final pesel = Pesel(peselString);
+
+  // Print the PESEL number
+  print(pesel);
+
+  // Check if the PESEL is valid
+  if (pesel.isValid) {
+    print("This PESEL is valid.");
+  } else {
+    print("This PESEL is not valid.");
+  }
+
+  // Check if the PESEL belongs to a male or female
+  if (pesel.isMale) {
+    print("This PESEL belongs to a male.");
+  } else if (pesel.isFemale) {
+    print("This PESEL belongs to a female.");
+  }
+
+  // Print the sex of the individual
+  print("Sex: ${pesel.sex}");
+
+  // Print the date of birth
+  print("Date of Birth: ${pesel.dateOfBirth}");
 }
